75a0af517e552769fe2e6e0f2789e7d38a35db94
jakubbelanec <90830434+jakubbelanec@users.noreply.github.com>
2023-11-15 23:33:24 +0100

    1

---
+++ linear/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] vyskaLidi = new int[50];
+Random random = new Random();
+for (int i = 0; i < vyskaLidi.Length; i++)
+{
+    vyskaLidi[i] = random.Next(40, 220);
+}
+
+Console.WriteLine("Všechny výšky:");
+foreach (var vyska in vyskaLidi)
+{
+    Console.Write(vyska + " ");
+}
+Console.WriteLine();
+
+
+Console.Write("Chci najít člověka s výškou: ");
+int hledanaVyska = Convert.ToInt32(Console.ReadLine());
+
+
+bool nalezeno = false;
+for (int i = 0; i < vyskaLidi.Length; i++)
+{
+    if (vyskaLidi[i] == hledanaVyska)
+    {
+        nalezeno = true;
+        Console.WriteLine("Nalezeno ==> " + hledanaVyska);
+    }
+    break;
+}
+if (!nalezeno)
+{
+    Console.WriteLine("Nenalezeno");
+}
